fc7b5edec98f2d15c5cfbe7cf0f59aca8f4d4548
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-02-09 10:21:14 +0530

    added API URL

--- src/api/auth/apiAuthSignin.js
+++ src/api/auth/apiAuthSignin.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { API_BASE_URL } from "../../utils/constants";
 
 export const apiAuthSignin = async (data) => {
-  const res = await axios.post(`http://localhost:8003/signin`, data);
+  const res = await axios.post(`${API_BASE_URL}/signin`, data);
   return res;
 };
--- src/api/auth/apiAuthSignup.js
+++ src/api/auth/apiAuthSignup.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 import { format } from "date-fns";
+import { API_BASE_URL } from "../../utils/constants";
 
 export const apiAuthSignup = async (data) => {
-    
-    const formData = {
-      ...data,
-      registered_at: format(new Date(), "dd-MM-yyyy"),
-    };
-  
-    const res = await axios.post(`http://localhost:8003/signup`, formData);
-
-    return res;
-   
+  const formData = {
+    ...data,
+    registered_at: format(new Date(), "dd-MM-yyyy"),
   };
+
+  const res = await axios.post(`${API_BASE_URL}/signup`, formData);
+
+  return res;
+};
