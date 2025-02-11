d8693a4672d9dd0b3bc20e4f8c031d15d2c029f1
randhavevaibhav <89928659+randhavevaibhav@users.noreply.github.com>
2025-02-11 20:51:29 +0530

    Update apiAuthSignin.js

--- src/api/auth/apiAuthSignin.js
+++ src/api/auth/apiAuthSignin.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { API_BASE_URL, LOCAL_API_BASE_URL } from "../../utils/constants";
 
 export const apiAuthSignin = async (data) => {
-  const res = await axios.post(`${LOCAL_API_BASE_URL}/signin`, data);
+  const res = await axios.post(`${API_BASE_URL}/signin`, data);
   return res;
 };
