1593155e0e038d6ff4374fd5016889c08164067d
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-02-17 14:33:42 +0530

    changed to remote api

--- src/services/rootAPI/api.js
+++ src/services/rootAPI/api.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 import { LOCAL_API_BASE_URL, API_BASE_URL } from "../../utils/constants";
 
+//for local ===>
+// export const axiosPrivate = axios.create({
+//   baseURL: `${LOCAL_API_BASE_URL}`,
+//   withCredentials: true,
+// });
+
+// export default axios.create({
+//   baseURL: `${LOCAL_API_BASE_URL}`,
+// });
+
+//for PROD
 export const axiosPrivate = axios.create({
-  baseURL: `${LOCAL_API_BASE_URL}`,
+  baseURL: `${API_BASE_URL}`,
   withCredentials: true,
 });
 
 export default axios.create({
-  baseURL: `${LOCAL_API_BASE_URL}`,
+  baseURL: `${API_BASE_URL}`,
 });
