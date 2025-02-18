d24b5a9cc6a27d672b62d7377269bea24f1c431c
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-02-18 20:33:10 +0530

    switched to prod api

--- src/services/rootAPI/api.js
+++ src/services/rootAPI/api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 import { LOCAL_API_BASE_URL, API_BASE_URL } from "../../utils/constants";
 
 //for local ===>
-export const axiosPrivate = axios.create({
-  baseURL: `${LOCAL_API_BASE_URL}`,
-  withCredentials: true,
-});
-
-export default axios.create({
-  baseURL: `${LOCAL_API_BASE_URL}`,
-});
-
-//for PROD
 // export const axiosPrivate = axios.create({
-//   baseURL: `${API_BASE_URL}`,
+//   baseURL: `${LOCAL_API_BASE_URL}`,
 //   withCredentials: true,
 // });
 
 // export default axios.create({
-//   baseURL: `${API_BASE_URL}`,
+//   baseURL: `${LOCAL_API_BASE_URL}`,
 // });
+
+//for PROD
+export const axiosPrivate = axios.create({
+  baseURL: `${API_BASE_URL}`,
+  withCredentials: true,
+});
+
+export default axios.create({
+  baseURL: `${API_BASE_URL}`,
+});
