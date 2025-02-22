12b4fb501c5f09e5bfce371930784bba320befcf
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-02-22 16:27:43 +0530

    added fallback img

--- src/pages/Fallback/Fallback.jsx
+++ src/pages/Fallback/Fallback.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import { MainLayout } from '../../components/MainLayout/MainLayout';
-import ErrorGIF from "../../assets/Error_GIF.gif"
-export const Fallback = ({error}) => {
+import React from "react";
+import { MainLayout } from "../../components/MainLayout/MainLayout";
+import ErrorGIF from "../../assets/Error_GIF.gif";
+export const Fallback = ({ error }) => {
   return (
     <MainLayout className={`mx-auto`}>
-        {/* <img src={ErrorGIF} alt="Error gif" /> */}
-        <div className='flex flex-col justify-center items-center border-black border error-cont'>
-        <h3 className='text-red-500'>Something went Wrong !!!</h3>
+      <img src={ErrorGIF} alt="Error gif" />
+      <div className="flex flex-col justify-center items-center border-black border error-cont">
+        <h3 className="text-red-500">Something went Wrong !!!</h3>
         <pre>{error.message}</pre>
-        </div>
-       
+      </div>
     </MainLayout>
-  )
-}
-
+  );
+};
