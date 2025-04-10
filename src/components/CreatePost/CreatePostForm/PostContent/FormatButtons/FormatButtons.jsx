c52b700af02c03a78aad91be6a2e347376439a58
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-04-10 20:25:02 +0530

    fixed import

--- src/components/CreatePost/CreatePostForm/PostContent/FormatButtons/FormatButtons.jsx
+++ src/components/CreatePost/CreatePostForm/PostContent/FormatButtons/FormatButtons.jsx
@@ -1,6 +1,7 @@
 import React, { memo } from "react";
 import { Button } from "../../../../common/Button/Button";
 import { usePostContext } from "../../../../../hooks/posts/usePostContext";
+import { saveLocalPostData } from "../../../../../utils/browser";
 
 const addBoldMarkdownChara = (text) => {
   const formattedText = ` **${text}** `;
@@ -35,92 +36,94 @@ const getSelectedText = ({ textarea }) => {
 };
 
 export const FormatButtons = memo(() => {
-    const { postDataRef, saveContentLocal } = usePostContext();
-  
-    const makeTextBold = () => {
-      const { selectionStart, selectionEnd, selectedText } = getSelectedText({
-        textarea: postDataRef.current.content,
+  const { postDataRef, saveContentLocal } = usePostContext();
+
+  const makeTextBold = () => {
+    const { selectionStart, selectionEnd, selectedText } = getSelectedText({
+      textarea: postDataRef.current.content,
+    });
+    const isAlreadyBold = selectedText.includes(`**`) ? true : false;
+    if (!isAlreadyBold) {
+      const boldText = addBoldMarkdownChara(selectedText);
+
+      const newText = replaceWithFormattedText({
+        originalText: postDataRef.current.content.value,
+        formattedText: boldText,
+        selectionStart,
+        selectionEnd,
       });
-      const isAlreadyBold = selectedText.includes(`**`) ? true : false;
-      if (!isAlreadyBold) {
-        const boldText = addBoldMarkdownChara(selectedText);
-  
-        const newText = replaceWithFormattedText({
-          originalText: postDataRef.current.content.value,
-          formattedText: boldText,
-          selectionStart,
-          selectionEnd,
-        });
-  
-        postDataRef.current.content.value = newText;
-        const contentVal = newText;
-           saveLocalPostData({
-            content:contentVal
-           });
-      }
-    };
-  
-    const makeTextItalic = () => {
-      const { selectedText, selectionEnd, selectionStart } = getSelectedText({
-        textarea: postDataRef.current.content,
+
+      postDataRef.current.content.value = newText;
+      const contentVal = newText;
+
+      console.log("new text ===> ", contentVal);
+      saveLocalPostData({
+        content: contentVal,
       });
-  
-      const isAlreadydItalic = selectedText.includes(`_`) ? true : false;
-      if (!isAlreadydItalic) {
-        const italicText = addItalicMarkDownChara(selectedText);
-        const newText = replaceWithFormattedText({
-          originalText: postDataRef.current.content.value,
-          formattedText: italicText,
-          selectionStart,
-          selectionEnd,
-        });
-  
-        postDataRef.current.content.value = newText;
-  
-        const contentVal = newText;
-           saveLocalPostData({
-            content:contentVal
-           });
-      }
-    };
-  
-    const addCodeBlock = () => {
-      const content = postDataRef.current.content.value;
-      const codeBlock = "```js\n\n```";
-      const newText = `${content}\n${codeBlock}`;
+    }
+  };
+
+  const makeTextItalic = () => {
+    const { selectedText, selectionEnd, selectionStart } = getSelectedText({
+      textarea: postDataRef.current.content,
+    });
+
+    const isAlreadydItalic = selectedText.includes(`_`) ? true : false;
+    if (!isAlreadydItalic) {
+      const italicText = addItalicMarkDownChara(selectedText);
+      const newText = replaceWithFormattedText({
+        originalText: postDataRef.current.content.value,
+        formattedText: italicText,
+        selectionStart,
+        selectionEnd,
+      });
+
       postDataRef.current.content.value = newText;
-    };
-  
-    const addHeading = () => {
-      const content = postDataRef.current.content.value;
-      const headingMarkDown = `## `;
-      let newText = "";
-      if (content) {
-        newText = `${content}\n${headingMarkDown}`;
-      } else {
-        newText = `${headingMarkDown}`;
-      }
-  
-      postDataRef.current.content.value = newText;
-    };
-    return (
-      <div className="flex gap-2 helpers mb-4">
-        <Button type="button" className={`font-bold`} onClick={makeTextBold}>
-          B
-        </Button>
-        <Button
-          type="button"
-          className={`font-bold italic`}
-          onClick={makeTextItalic}
-        >
-          I
-        </Button>
-        <Button type="button" onClick={addCodeBlock}>
-          JS
-        </Button>
-        <Button type="button" onClick={addHeading}>
-          H
-        </Button>
-      </div>
-    );
-  })
+
+      const contentVal = newText;
+      saveLocalPostData({
+        content: contentVal,
+      });
+    }
+  };
+
+  const addCodeBlock = () => {
+    const content = postDataRef.current.content.value;
+    const codeBlock = "```js\n\n```";
+    const newText = `${content}\n${codeBlock}`;
+    postDataRef.current.content.value = newText;
+  };
+
+  const addHeading = () => {
+    const content = postDataRef.current.content.value;
+    const headingMarkDown = `## `;
+    let newText = "";
+    if (content) {
+      newText = `${content}\n${headingMarkDown}`;
+    } else {
+      newText = `${headingMarkDown}`;
+    }
+
+    postDataRef.current.content.value = newText;
+  };
+  return (
+    <div className="flex gap-2 helpers mb-4">
+      <Button type="button" className={`font-bold`} onClick={makeTextBold}>
+        B
+      </Button>
+      <Button
+        type="button"
+        className={`font-bold italic`}
+        onClick={makeTextItalic}
+      >
+        I
+      </Button>
+      <Button type="button" onClick={addCodeBlock}>
+        JS
+      </Button>
+      <Button type="button" onClick={addHeading}>
+        H
+      </Button>
+    </div>
+  );
+});
