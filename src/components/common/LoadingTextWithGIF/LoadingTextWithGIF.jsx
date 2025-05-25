c8c4df8ce491564b42306e039a149282569714b5
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-05-25 13:26:34 +0530

    added new animation for loading

--- src/components/common/LoadingTextWithGIF/LoadingTextWithGIF.jsx
+++ src/components/common/LoadingTextWithGIF/LoadingTextWithGIF.jsx
@@ -1,20 +1,14 @@
-import { Spinner } from "../Spinner/Spinner";
-import LoadingGIFDark from "../../../assets/loading-bird-dance-dark.gif";
-import LoadingGIFLight from "../../../assets/loading-bird-dance-light.gif";
-import { useThemeContext } from "@/hooks/Theme/useThemeContext";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 export const LoadingTextWithGIF = ({ children }) => {
-  const { theme } = useThemeContext();
-  const isDark = theme === "dark";
   return (
-    <div className=" flex flex-col justify-center items-center w-full h-[600px] gap-4 ">
-      <div className="">
-        {isDark ? (
-          <img src={LoadingGIFDark} alt={"loading gif"} className="" />
-        ) : (
-          <img src={LoadingGIFLight} alt={"loading gif"} className="" />
-        )}
-      </div>
-      <p className="text-fs_base font-medium">{children}</p> <Spinner />
+    <div className="flex items-center flex-col min-h-screen justify-center">
+      <DotLottieReact
+        src="https://lottie.host/be01b52f-376b-4585-858f-d1b47b82676e/cayMvU6wb4.lottie"
+        loop
+        autoplay
+        className="h-[200px]"
+      />
+      <p className="text-fs_base font-medium">{children}</p>
     </div>
   );
 };
