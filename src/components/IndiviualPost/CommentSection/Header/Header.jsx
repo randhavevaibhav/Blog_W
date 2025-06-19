14ef683ea86e674de98c6d3daba2f4af40434bf1
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-06-19 20:32:13 +0530

    cosmetic fix

--- src/components/IndiviualPost/CommentSection/Header/Header.jsx
+++ src/components/IndiviualPost/CommentSection/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React, { memo } from "react";
 import { CommentSort } from "./CommentSort/CommentSort";
 
-export const Header = memo(({ totalComments ,handleCmtSort}) => {
+export const Header = memo(({ totalComments, handleCmtSort }) => {
   // console.log("header re-render")
   return (
-    <div className="flex items-center md:gap-4 gap-2 mb-2">
+    <div className="flex items-center md:gap-4 gap-2 mb-2 md:flex-row flex-col">
       <header className="mb-2">
         <h3 className="flex gap-3 text-fs_xl font-semibold">
           Comments
@@ -13,7 +13,7 @@ export const Header = memo(({ totalComments ,handleCmtSort}) => {
           </span>
         </h3>
       </header>
-       <CommentSort handleCmtSort={handleCmtSort} />
+      <CommentSort handleCmtSort={handleCmtSort} />
     </div>
   );
-})
+});
