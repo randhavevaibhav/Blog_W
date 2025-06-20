c4add525b74f27b5e0bc27ae6624fc13c88e76c6
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-06-20 19:26:05 +0530

    fix size

--- src/components/common/UserAvatar/UserAvatar.jsx
+++ src/components/common/UserAvatar/UserAvatar.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
+import React from "react";
 import { IoPersonCircleSharp } from "react-icons/io5";
 
 const avatarSizeList = {
-  small:{
-   size: `w-[40px]`,
-   fallbackSize:`40px`
+  small: {
+    size: `w-[40px]`,
+    fallbackSize: `40px`,
   },
-  medium:{
-    size: `w-30`,
-    fallbackSize:`50px`
-   },
-  large:{
+  medium: {
+    size: `w-[30px]`,
+    fallbackSize: `50px`,
+  },
+  large: {
     size: `w-[90px]`,
-    fallbackSize:`90px`
-   }
-}
-export const UserAvatar = ({userProfileImg,avatarSize='medium'}) => {
+    fallbackSize: `90px`,
+  },
+};
+export const UserAvatar = ({ userProfileImg, avatarSize = "medium" }) => {
   return (
-    <div
-  
-    className=""
-  >
-    {!userProfileImg ? (
-       <div className={`${avatarSizeList[avatarSize].size} mr-2 flex justify-center rounded-full `}>
-      <IoPersonCircleSharp size={avatarSizeList[avatarSize].fallbackSize} className='p-1' />
-      </div>
-    ) : (
-      <div className={`${avatarSizeList[avatarSize].size} mr-2`}>
-        <img
-          src={userProfileImg}
-          alt={`user profile image`}
-          className="object-cover aspect-square w-full rounded-full"
-          id='profileImg'
-        />
-      </div>
-    )}
-  
-  </div>
-  )
-}
+    <div className="">
+      {!userProfileImg ? (
+        <div
+          className={`${avatarSizeList[avatarSize].size} mr-2 flex justify-center rounded-full `}
+        >
+          <IoPersonCircleSharp
+            size={avatarSizeList[avatarSize].fallbackSize}
+            className="p-1"
+          />
+        </div>
+      ) : (
+        <div className={`${avatarSizeList[avatarSize].size} mr-2`}>
+          <img
+            src={userProfileImg}
+            alt={`user profile image`}
+            className="object-cover aspect-square w-full rounded-full"
+            id="profileImg"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
