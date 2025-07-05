5f2eb85409fc742c8a5317e829b5613700563f58
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-07-05 23:42:44 +0530

    added prefetching

--- src/components/common/Follower/Follower.jsx
+++ src/components/common/Follower/Follower.jsx
@@ -1,11 +1,21 @@
 import { UserAvatar } from "@/components/common/UserAvatar/UserAvatar";
+import { usePrefetch } from "@/hooks/prefetch/usePrefetch";
 import React, { forwardRef } from "react";
 import { Link } from "react-router-dom";
 
 export const Follower = forwardRef(
   ({ id, name, email, profileImgURL }, ref) => {
+    const { preFetchUserInfo } = usePrefetch();
     return (
-      <div className="" ref={ref}>
+      <div
+        className=""
+        ref={ref}
+        onMouseOver={() => {
+          preFetchUserInfo({
+            userId: id,
+          });
+        }}
+      >
         <div className="flex flex-col gap-2 items-center bg-card-bg rounded-md py-4">
           <Link to={`/userprofile/${id}`}>
             <UserAvatar avatarSize="large" userProfileImg={profileImgURL} />
