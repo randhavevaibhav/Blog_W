75f93e037d7f93a02ad1b8d0d10b2e80f45e8f1f
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-07-27 18:58:33 +0530

    fixed tagged post

--- src/pages/TaggedPosts/TaggedPosts.jsx
+++ src/pages/TaggedPosts/TaggedPosts.jsx
@@ -41,7 +41,7 @@ const TaggedPosts = () => {
       <MainLayout
         className={` md:mx-auto max-w-[700px] mb-0 p-4 bg-bg-primary`}
       >
-        <div className="flex flex-col space-y-3">
+        <div className="max-w-[50rem] mx-auto mb-6 flex flex-col space-y-3">
           <PostArticleSkeleton count={4} />
         </div>
       </MainLayout>
