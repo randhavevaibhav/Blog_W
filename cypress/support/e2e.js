90c996d947db414ed535257c07201f95aedc54de
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-12-14 10:25:48 +0530

    debug

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,20 +1,25 @@
-Cypress.Commands.add('getBySel', (selector, ...args) => {
-  return cy.get(`[data-test=${selector}]`, ...args)
-})
-
-Cypress.Commands.add('getBySelLike', (selector, ...args) => {
-  return cy.get(`[data-test*=${selector}]`, ...args)
-})
-
-Cypress.Commands.add('checkToastMessage', ({message},...args) => {
-  cy.get(`#_rht_toaster [role='status']`,...args).should('contain', message);
+//for github actions
+beforeEach(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
 });
 
-
-Cypress.Commands.add('checkPathEqTo', ({path},...args) => {
-  cy.location("pathname",...args).should("eq", path);
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+  return cy.get(`[data-test=${selector}]`, ...args);
 });
 
-Cypress.Commands.add('checkPathInc', ({path},...args) => {
-  cy.location("pathname",...args).should("include", path);
+Cypress.Commands.add("getBySelLike", (selector, ...args) => {
+  return cy.get(`[data-test*=${selector}]`, ...args);
+});
+
+Cypress.Commands.add("checkToastMessage", ({ message }, ...args) => {
+  cy.get(`#_rht_toaster [role='status']`, ...args).should("contain", message);
+});
+
+Cypress.Commands.add("checkPathEqTo", ({ path }, ...args) => {
+  cy.location("pathname", ...args).should("eq", path);
+});
+
+Cypress.Commands.add("checkPathInc", ({ path }, ...args) => {
+  cy.location("pathname", ...args).should("include", path);
 });
