1f026b8c00bf2f1f359f14304ce8d2dae597c53f
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-12-14 15:50:49 +0530

    corrected yml

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   e2e: {
     video: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     setupNodeEvents(on, config) {
       const options = {
         webpackOptions: {
